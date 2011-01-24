88053cb9710830c52e576fab16c906942269d688
Kaspar Schiess <kaspar.schiess@geo.uzh.ch>
2011-01-24 16:50:06 +0100

    + Repeats http parsing ten times to increase precision

--- benchmarks/002-http_parser.rb
+++ benchmarks/002-http_parser.rb
@@ -140,7 +140,8 @@ class RequestParser < Parslet::Parser
   root :request
 end
 
-RequestParser.new.parse(<<REQUEST)
+10.times do
+  RequestParser.new.parse(<<-REQUEST)
 GET /search?q=test&hl=en&fp=1&cad=b&tch=1&ech=1&psi=DBQ4Te_qCI2Y_QaIuPSTCA12955207804903 HTTP/1.1
 Host: www.google.com
 Referer: http://www.google.com/
@@ -152,4 +153,5 @@ Accept-Language: en-US,en;q=0.8
 Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3
 Cookie: NID=43=bgvZmm1C00aC41wQA0Yl5lVPEJZerwnK9LYDFo4Ph9_qBZFfbwT-auI64LZzdquh8StFriEuQfhrIgf_GlVd9erjOGppXZISHpoFgdiUUfpTqUbKC8gbfNh09eZXmcK7; PREF=ID=c28d27fb5ff1280b:U=fedcd44ca2fdef4f:FF=0:LD=en:CR=2:TM=1295517030:LM=1295517030:S=D36Ccqf-FQ78ZWE7
 
-REQUEST
+  REQUEST
+end
